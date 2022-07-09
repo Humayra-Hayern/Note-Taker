067b12afb896511e0e59eeaa62854f11d1f89948
Humayra Hayern <hayern786@hotmail.com>
2022-07-09 17:21:33 +0100

    testing

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const noteData = require("./db/db.json");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static middlewear
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
