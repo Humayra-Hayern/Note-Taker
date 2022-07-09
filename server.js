c7a5ff40decf0dc6441706609b4ab3e2f91c2ee1
Humayra Hayern <hayern786@hotmail.com>
2022-07-09 17:43:45 +0100

    test

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
