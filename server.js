03ad2ba18d81a75c91ae9b622021413b57175594
Humayra Hayern <hayern786@hotmail.com>
2022-07-10 00:05:34 +0100

    fixed server issue

--- server.js
+++ server.js
@@ -1,54 +1,70 @@
-const fs = require("fs");
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
-const uuid = require("uuid");
+const noteDatabase = require("./db/db.json");
+const uniqid = require("uniqid");
 
-let noteData = require("./db/db.json");
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
-
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
+// Setting the static folder
 app.use(express.static("public"));
 
+// Loads index.html when the page first loads
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+// Gets the notes.html page
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+// API Routes
+
+// GET /api/notes read the db.json file and return all saved notes as JSON.
 app.get("/api/notes", (req, res) => {
-  res.json(noteData);
+  res.json(noteDatabase);
 });
 
+// POST /api/notes receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 app.post("/api/notes", (req, res) => {
   const newNote = {
+    id: uniqid(),
     title: req.body.title,
     text: req.body.text,
-    id: uuid.v4(),
   };
 
-  noteData.push(newNote);
-  res.json(noteData);
+  // push the note to db.json
+  noteDatabase.push(newNote);
 
-  fs.writeFile("db/db.json", JSON.stringify(noteData), (err) => {
-    if (err) throw err;
-  });
+  // write to the db.json file to update the database
+  fs.writeFile("./db/db.json", JSON.stringify(noteDatabase), (err) =>
+    err ? console.log(err) : console.log("success")
+  );
+
+  // returns the note to the client
+  res.json(noteDatabase);
 });
 
+// DELETE /api/notes/:id receives a query parameter that contains the id of a note to delete
 app.delete("/api/notes/:id", (req, res) => {
-  let { id } = req.params;
-  noteData = noteData.filter((note) => note.id !== id);
-  res.send(`User with the id ${id} has been deleted from the database`);
-  fs.writeFile("db/db.json", JSON.stringify(noteData), (err) => {
-    if (err) throw err;
-  });
+  let noteId = req.params.id;
+  let index = noteDatabase.findIndex((note) => note.id === noteId);
+  let deletedNote = noteDatabase.splice(index, 1);
+  res.send(deletedNote);
+
+  // re-writes the file with updated database
+  fs.writeFile("./db/db.json", JSON.stringify(noteDatabase), (err) =>
+    err ? console.log(err) : console.log("success")
+  );
 });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
