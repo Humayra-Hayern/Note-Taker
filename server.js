cc27df5c965916f2d29b6b68c30c75f4548470ef
Humayra Hayern <hayern786@hotmail.com>
2022-07-09 18:28:49 +0100

    refactored code

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const noteData = require("./db/db.json");
+const noteDatabase = require("./db/db.json");
+const uniqid = require("uniqid");
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,13 +12,59 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static middlewear
+// Setting the static folder
 app.use(express.static("public"));
 
+// Loads index.html when the page first loads
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+// Gets the notes.html page
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+// API Routes
+
+// GET /api/notes read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", (req, res) => {
+  res.json(noteDatabase);
+});
+
+// POST /api/notes receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+app.post("/api/notes", (req, res) => {
+  const newNote = {
+    id: uniqid(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  // push the note to db.json
+  noteDatabase.push(newNote);
+
+  // write to the db.json file to update the database
+  fs.writeFile("./db/db.json", JSON.stringify(noteDatabase), (err) =>
+    err ? console.log(err) : console.log("success")
+  );
+
+  // returns the note to the client
+  res.json(noteDatabase);
+});
+
+// DELETE /api/notes/:id receives a query parameter that contains the id of a note to delete
+app.delete("/api/notes/:id", (req, res) => {
+  let noteId = req.params.id;
+  let index = noteDatabase.findIndex((note) => note.id === noteId);
+  let deletedNote = noteDatabase.splice(index, 1);
+  res.send(deletedNote);
+
+  // re-writes the file with updated database
+  fs.writeFile("./db/db.json", JSON.stringify(noteDatabase), (err) =>
+    err ? console.log(err) : console.log("success")
+  );
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
